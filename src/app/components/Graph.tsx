c8d2d0685e81bb41d664d1ab52c8b70285d2c544
Alexandra Chernova <estelelendil@mail.ru>
2023-10-08 21:56:46 +0300

    correct interval XAxis

--- src/app/components/Graph.tsx
+++ src/app/components/Graph.tsx
@@ -26,9 +26,21 @@ export default function Graph({ data }: { data: RechartsGraphData }) {
       <Filter filter={filter} setFilter={setFilter} />
       <div className="bg-[#fff2fe] rounded-[30px] w-[1000px] p-4">
         <BarChart data={data[filter]} height={300} width={900} margin={{ top: 10 }}>
-          <XAxis axisLine={false} dataKey="label" stroke="#000aff" {...xyProps} interval={filter === "month" ? 4 : 0} />
+          <XAxis
+            axisLine={false}
+            dataKey="label"
+            stroke="#000aff"
+            {...xyProps}
+            tickFormatter={(value, index) => {
+              if (filter !== "month") {
+                return value;
+              } else {
+                return index === 0 || (index + 1) % 5 === 0 ? value : " ";
+              }
+            }}
+          />
           <YAxis {...xyProps} axisLine={false} />
-          <Bar isAnimationActive={false} dataKey="data" radius={3} {...graphProps} minPointSize={2} />
+          <Bar isAnimationActive={false} dataKey="data" radius={2} {...graphProps} minPointSize={2} />
         </BarChart>
       </div>
     </div>
